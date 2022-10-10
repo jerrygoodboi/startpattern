573fba051d074499b948697527e4514c8153f71e
Jerry <rabbitron295@gmail.com>
2022-10-10 22:19:23 +0530

    Added pattern.c file

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+        int i,k=4,j,n;
+        for(n=1;n<=k;n++){
+                i=k-n;
+                while(i>0){
+                printf(" ");
+                i--;
+                }
+for(j=0;j<2*n-1;j++)
+        {
+printf("*");
+
+        }
+printf("\n");
+        }}
